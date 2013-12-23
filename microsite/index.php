ee1c533ddc4a1dd393261ebb9a13c292a086fcb9
dongvanluu <luudv@dhwebsoft.com>
2013-12-23 15:52:17 +0700

    first commit

---
+++ microsite/index.php
@@ -0,0 +1,7 @@
+<?php include_once "header.php";?>
+<?php include_once "navsticky.php";?>
+<?php include_once "testimonial.php";?>
+<img src="images/rule.png">
+<?php include_once "modifferent.php"; ?>
+<img src="images/rule.png">
+<?php include_once "footer.php";?>
---
+++ microsite/testimonial.php
@@ -0,0 +1,15 @@
+<div id="div_testimonial" class="row">
+	<div class="col-md-4">
+		<div id="left_text_testimonial" class="font-green-mormal">
+			<b>Want to see who else is benefiting from Managed Office?</b>
+		</div>
+	</div>
+	<div class="col-md-8">
+		<div id="left_arrow_testimonial"><img src="images/left_arrow.png"></div>
+		<div id="right_text_testimonial" class="font-white-mormal">
+			<b>"With Managed Office, we're more efficient, and more productive. For us, it was the shortcut to fast-forward"</b><br>
+            <i>Dale Griffith, Western Trucking</i>
+		</div>
+        <div id="right_arrow_testimonial"><img src="images/right_arrow.png"></div>
+	</div>
+</div>
