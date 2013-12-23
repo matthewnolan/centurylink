a02513014934486504f8cee3c94709f14ef81663
dongvanluu <luudv@dhwebsoft.com>
2013-12-23 16:32:49 +0700

    add images

---
+++ microsite/mobusiness.php
@@ -0,0 +1,22 @@
+<div id="MOBusiness">
+	<div class="content-wrapper">
+		<div class="row" style="padding-bottom:100px;">
+			<div class="col-md-6">
+				<div id="div_left_modifferent">
+					<h2>What makes Managed Office different?</h2>
+			        <span class="font-gray-mormal"><b>Managed Office is more than just a bundle of products and services. When you have Managed Office, CenturyLink implements, maintains and upgrades a huge amount of advanced business information technology, so that you don't have to worry about it. You get one provider, one network, and one number to call for White Glove Service...</b></span><br><br>
+			        <b>With Managed Office, it's all-in-one and done.</b>
+			    </div>
+		    </div>
+		    <div class="col-md-6">
+				<div id="div_right_modifferent">
+					<img src="images/diagram_modifferent.png">
+			    </div>
+		    </div>
+	    </div>
+	    
+	    <div id="down_arrow_modifferent">
+    		<img src="images/down_arrow.png">&nbsp;&nbsp;&nbsp;IS MANAGED OFFICE RIGHT FOR YOUR BUSINESS
+		</div>
+	</div>
+</div>
