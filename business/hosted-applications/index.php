5774d55ebfbf12f9c3c5bfd499c1c7dde886c812
Matthew Nolan <matthewnolan415@gmail.com>
2014-01-04 09:03:48 -0500

    wtf make work

--- business/hosted-applications/index.php
+++ business/hosted-applications/index.php
@@ -1,4 +1,29 @@
 <?php
+	#############
+	# THIS PART IS PAGE LOADER
+	#############
+	
+	include_once "../../config.php";
+	
+	# include other files
+	$redirect_url = @$_SERVER['REDIRECT_URL'];
+	if(!empty($redirect_url)) {
+		$file = str_replace(ROOT_URL, '', $redirect_url);
+		if(strpos($file, '.php') === FALSE) {
+			$file = rtrim($file, '/') . '/index.php';
+		}
+		if(file_exists(ROOT_PATH . $file)){
+			return include ROOT_PATH . $file;
+		}
+	}
+	
+	#############
+	# END PAGE LOADER
+	#############
+?>
+
+
+<?php
 	page_header("CenturyLink Cloud | Business Owner | Hosted Applications");
 ?>
 
