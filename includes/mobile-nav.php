0a25f0791499e8e15f4003a6366aa2a85fee4335
Nam Tran Huu <namth@hdwebsoft.com>
2014-01-04 11:03:25 +0700

    remove blog link from grey side nav

--- includes/mobile-nav.php
+++ includes/mobile-nav.php
@@ -20,38 +20,6 @@
 					<li><a href="<?php echo ROOT_URL;  ?>/it/data-backup/" title="">Data Backup</a></li>
 				</ul>
 			</li>
-			<li>
-				<ul>
-					<li>View our Blog</li>
-					<li class="social-icon">
-						<a class="twitter" href="https://twitter.com/CenturyLinkBiz" title="Follow us on Twitter">
-							<span class="fa-stack fa-lg">
-								<i class="fa fa-circle fa-stack-2x"></i>
-								<i class="fa icon-twitter fa-stack-1x fa-inverse"></i>
-							</span>
-							</a>
-							<a class="facebook" href="https://www.facebook.com/CenturyLinkBusiness" title="Follow us on Facebook">
-								<span class="fa-stack fa-lg">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-								</span>
-							</a>
-							<a class="google-plus" href="https://plus.google.com/u/0/117080893855557903730/posts" title="Follow us on Google Plus">
-								<span class="fa-stack fa-lg">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-google-plus fa-stack-1x fa-inverse"></i>
-								</span>
-							</a>
-							<a class="linkedin" href="http://www.linkedin.com/company/1434913?trk=tyah" title="Follow us on Linkedin">
-								<span class="fa-stack fa-lg">
-									<i class="fa fa-circle fa-stack-2x"></i>
-									<i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
-								</span>
-						</a>
-					</li>
-				</ul>
-			</li>
-		
 		</ul>
 		
 		<a id="mobilenav-button" href="#mobilenav">
