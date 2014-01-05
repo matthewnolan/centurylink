1552f81088a1f7842c572ddeb63f66220dcc7c06
Son Dat Giang <datgs@hdwebsoft.com>
2014-01-05 08:25:27 +0700

    Business Databackup content update

--- business/data-backup/b-data.php
+++ business/data-backup/b-data.php
@@ -25,170 +25,201 @@
 		<div class="padding-fix">
 			<div class="row">
 				<div class="col-sm-6 copy-block">
-					<h3 class="block-title">
-						Back up your files like a pro. No experience required.
-					</h3>
-					<p class="block-content">
-						With CenturyLink Cloud Solutions, your employees' files are backed up automatically. And with our easy-to-use management console, you'll get the admin tools you need to install and schedule backups or delete old and archived files--letting you literally set it and forget it.
-					</p>
+					<h3 class="block-title">Back up your files like a pro. No
+						experience required.</h3>
+					<p class="block-content">With CenturyLink Cloud Solutions, your
+						employees' files are backed up automatically. And with our
+						easy-to-use Management Console, you'll get the admin tools you
+						need to install and schedule backups or delete old and archived
+						files—letting you literally set it and forget it.</p>
 				</div>
 				<div class="col-sm-6 image-block">
-				<img src="<?php echo ROOT_URL;  ?>/images/fpo3_2x.png" width="450" alt="FPO"/>
+					<img src="<?php echo ROOT_URL;  ?>/images/fpo3_2x.png" width="450"
+						alt="FPO" />
 				</div>
 			</div>
-			
+
 			<div class="block-seperator"></div>
-			<hr class="block-hr"/>
-			
+			<hr class="block-hr" />
+
 			<div class="row sticky-padding" id="safe">
 				<div class="col-sm-6 image-block hidden-xs">
-					<img src="<?php echo ROOT_URL;  ?>/images/fpo4_2x.png" width="380" alt=""/>
+					<img src="<?php echo ROOT_URL;  ?>/images/fpo4_2x.png" width="380"
+						alt="" />
 				</div>
 				<div class="col-sm-6 copy-block">
-					<h3 class="block-title">
-						The business world doesn’t run smoothly. Your data backups can.
-					</h3>
-					<p class="block-content">
-						Here's one worry you can cross off your list. With our 99.95% uptime service level agreement, you can rest assured that your files are always safe, securely accessible and automatically backed up offsite.
-					</p>
+					<h3 class="block-title">The business world doesn’t run smoothly.
+						Your data backups can.</h3>
+					<p class="block-content">Here's one worry you can cross off your
+						list. With our 99.95% uptime service level agreement, you can rest
+						assured that your files are always safe, securely accessible and
+						automatically backed up offsite. Plus, we support the most
+						stringent levels of data compliance across the healthcare, legal
+						and financial industries.</p>
 					<p class="block-images">
-						<img src="<?php echo ROOT_URL;  ?>/images/bdb-logo3.jpg" alt="LOGO"/>
-						<img src="<?php echo ROOT_URL;  ?>/images/bdb-logo1.jpg" alt="LOGO"/>
-						<img src="<?php echo ROOT_URL;  ?>/images/bdb-logo2.jpg" alt="LOGO"/>
-						<img src="<?php echo ROOT_URL;  ?>/images/bdb-logo4.jpg" alt="LOGO"/>
-						<img src="<?php echo ROOT_URL;  ?>/images/bdb-logo5.jpg" alt="LOGO"/>
+						<img src="<?php echo ROOT_URL;  ?>/images/bdb-logo3.jpg"
+							alt="LOGO" /> <img
+							src="<?php echo ROOT_URL;  ?>/images/bdb-logo1.jpg" alt="LOGO" />
+						<img src="<?php echo ROOT_URL;  ?>/images/bdb-logo2.jpg"
+							alt="LOGO" /> <img
+							src="<?php echo ROOT_URL;  ?>/images/bdb-logo4.jpg" alt="LOGO" />
+						<img src="<?php echo ROOT_URL;  ?>/images/bdb-logo5.jpg"
+							alt="LOGO" />
 					</p>
 				</div>
 				<div class="col-sm-6 image-block visible-xs">
-					<img src="<?php echo ROOT_URL;  ?>/images/fpo4_2x.png" width="380" alt=""/>
+					<img src="<?php echo ROOT_URL;  ?>/images/fpo4_2x.png" width="380"
+						alt="" />
 				</div>
 			</div>
-			
+
 			<div class="block-seperator"></div>
-			<hr class="block-hr"/>
-			
+			<hr class="block-hr" />
+
 			<div class="row sticky-padding" id="ffb">
 				<div class="col-sm-7 copy-block">
-					<h3 class="block-title">
-						Onboard effortlessly and get started today.
+					<h3 class="block-title">Onboard effortlessly and get started today.
 					</h3>
-					<p class="block-content">
-						Nothing should stand in the way of your business taking full advantage of our cloud services,
-						or getting the convenience and security that comes with backing up your data to the Cloud. With FREE FastForward Onboarding&#8480;,
-						getting started is easy. Just choose from one of our three onboarding options based on your timing and budget, and get started with ease.
-					</p>
-					
+					<p class="block-content">Nothing should stand in the way of your
+						business taking full advantage of our Cloud Services, or getting
+						the convenience and security that comes with backing up your data
+						to the cloud. With FREE FastForward Onboarding, getting started is
+						easy. Just choose from one of our three onboarding options based
+						on your timing and budget, and get started with ease.</p>
+
 				</div>
 				<div class="col-sm-5 image-block">
-					<img id="onboard-image" src="<?php echo ROOT_URL;  ?>/images/onboard_2x.png" width="374" alt="ONBOARD"/>
+					<img id="onboard-image"
+						src="<?php echo ROOT_URL;  ?>/images/onboard_2x.png" width="374"
+						alt="ONBOARD" />
 				</div>
 			</div>
-			
+
 			<div class="block-seperator"></div>
-			
+
 			<div class="row onboard3col">
 				<div class="col-sm-4">
-					<img src="<?php echo ROOT_URL;  ?>/images/gears.png" alt="GEARS"/>
+					<img src="<?php echo ROOT_URL;  ?>/images/gears.png" alt="GEARS" />
 					<div class="onboard-title">Do it Yourself</div>
 					<div class="onboard-content readmore">
-						Maybe you want the flexibility to set up and activate our Cloud services on your own.
-						<span class="3dot">...</span><div class="details"></div>
-						Well, we'll still make it easy. Using nothing more than an Internet connection, you can start backing up your files to the Cloud quickly, whenever you want.  CenturyLink provides easy access to your data with our intuitive online Management Console.
-					
+						Maybe you want the flexibility to set up and activate our Cloud
+						Services on your own. <span class="3dot">...</span>
+						<div class="details"></div>
+						Well, we'll still make it easy. Using nothing more than an
+						Internet connection, you can start backing up your files to the
+						cloud quickly, whenever you want. CenturyLink provides easy access
+						to your data with our intuitive online Management Console.
+
 					</div>
 				</div>
 				<div class="col-sm-4 ">
-					<img src="<?php echo ROOT_URL;  ?>/images/talk.png" alt="TALK"/>
+					<img src="<?php echo ROOT_URL;  ?>/images/talk.png" alt="TALK" />
 					<div class="onboard-title">We Help You Get Started</div>
 					<div class="onboard-content readmore">
-						With our guided activation, one of our Cloud experts will walk you through the
-						<span class="3dot">...</span><div class="details"></div>
-						 initial steps of setup in order to make sure your business is taking full advantage of the Cloud and can back up all of your files safely and securely.
+						With our guided activation, one of our cloud experts will walk you
+						through the <span class="3dot">...</span>
+						<div class="details"></div>
+						initial steps of setup in order to make sure your business is
+						taking full advantage of the cloud and can back up all of your
+						files safely and securely.
 						<div class="new-fix-height4 hidden-xs hidden-sm"></div>
 					</div>
 				</div>
 				<div class="col-sm-4 ">
-					<img src="<?php echo ROOT_URL;  ?>/images/join.png" alt="Join"/>
+					<img src="<?php echo ROOT_URL;  ?>/images/join.png" alt="Join" />
 					<div class="onboard-title">We Take Care of Every Step</div>
 					<div class="onboard-content readmore">
-						Need additional support? A CenturyLink Cloud expert will work closely with you to
-						<span class="3dot">...</span><div class="details"></div>
-						customize a plan that meets all your business needs and data backup requirements while getting you to the Cloud quickly and affordably.
+						Need additional support? A CenturyLink cloud expert will work
+						closely with you to <span class="3dot">...</span>
+						<div class="details"></div>
+						customize a plan that meets all your business needs and data
+						backup requirements while getting you to the cloud quickly and
+						affordably.
 						<div class="new-fix-height4 hidden-xs hidden-sm"></div>
 					</div>
 				</div>
 			</div>
-			
+
 			<div class="it-present">
 				<h3>Want your own IT staff?</h3>
 				<p>
-					Right now you can upgrade to our <strong>Remote IT Support Pack for only $19.99/month.</strong><br/>
-					CenturyLink will handle maintenance, networking, and troubleshooting for all devices.
+					Right now, you can upgrade to our <strong>Remote IT Support Pack
+						for only $19.99/month.</strong><br /> CenturyLink will handle
+					maintenance, networking, and troubleshooting for all devices.
 				</p>
 				<p>
-					<strong>Call 800.361.0833</strong> to learn more about our remote IT support pack.
+					<strong>Call 800.361.0833</strong> to learn more about our remote
+					IT support pack.
 				</p>
 			</div>
-			
-			
+
+
 			<div class="block-seperator"></div>
-			<hr class="block-hr"/>
-			
-			<div class="row sticky-padding"  id="pricing">
+			<hr class="block-hr" />
+
+			<div class="row sticky-padding" id="pricing">
 				<div class="col-sm-6 image-block hidden-xs">
-					<img src="<?php echo ROOT_URL;  ?>/images/fpo5_2x.png" width="360" alt=""/>
+					<img src="<?php echo ROOT_URL;  ?>/images/fpo5_2x.png" width="360"
+						alt="" />
 				</div>
 				<div class="col-sm-6 copy-block">
-					<h3 class="block-title">
-						The solutions are in the cloud. The pricing is down to earth.
-					</h3>
-					<p class="block-content">
-						When it comes to backing up your critical data, every business has its own needs.
-						Unlike traditional on-site data backup solutions, our pricing plans are applied to individual users,
-						and can be scaled up and down as your business evolves--so you never have to pay for what you're not using.
-					</p>
-					
+					<h3 class="block-title">The solutions are in the cloud. The pricing
+						is down to earth.</h3>
+					<p class="block-content">When it comes to backing up your critical
+						data, every business has its own needs. Unlike traditional on-site
+						data backup solutions, our pricing plans are applied to individual
+						users and can be scaled up and down as your business evolves—so
+						you never have to pay for what you're not using.</p>
+
 				</div>
 				<div class="col-sm-6 image-block visible-xs">
-					<img src="<?php echo ROOT_URL;  ?>/images/fpo5_2x.png" width="360" alt=""/>
+					<img src="<?php echo ROOT_URL;  ?>/images/fpo5_2x.png" width="360"
+						alt="" />
 				</div>
 			</div>
-					
+
 			<div class="cost">
 				<div class="cost-title">
 					<span class="hidden-sm hidden-xs"></span>
-					<h3>Our flexible model allows you to scale your storage so <br/> you only pay for just what you need.</h3>
+					<h3>
+						Our flexible model allows you to scale your storage so <br /> you
+						only pay for just what you need.
+					</h3>
 					<span class="hidden-sm hidden-xs"></span>
 				</div>
 				<div class="clearfix"></div>
-				<img src="<?php echo ROOT_URL;  ?>/images/bs-pricing.png" alt=""/>
+				<img src="<?php echo ROOT_URL;  ?>/images/bs-pricing.png" alt="" />
 				<div class="cost-more">
-					Call us so we can show you how CenturyLink can reduce your costs.<br/>
-					<a href="tel:800.361.0833"><strong>800.361.0833</strong></a>
+					Call us so we can show you how CenturyLink can reduce your costs.<br />
+					<a href="tel:800.361.0833"><strong>800.361.0833</strong> </a>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
 <div class="block-seperator"></div>
-<hr class="home-hr"/>
+<hr class="home-hr" />
 <div class="video">
 	<div class="container wrapper">
 		<div class="padding-fix">
 			<div class="row video-container">
 				<div class="video-left col-sm-7 hidden-xs">
-					<iframe width="560" height="315" src="//www.youtube.com/embed/PfOEqfbQ6gE" frameBorder="0" allowfullscreen></iframe>
+					<iframe width="560" height="315"
+						src="//www.youtube.com/embed/PfOEqfbQ6gE" frameBorder="0"
+						allowfullscreen></iframe>
 				</div>
 				<div class="video-right col-sm-5">
 					<div>
 						<h3>Learn why you can depend on the cloud through thick and thin.</h3>
-						<p>
-							Every business faces adversity&#8212;but when you have the cloud backing up your data, surviving disasters great and small is a whole lot easier. Watch the video to find out more.
-						</p>
+						<p>Every business faces adversity&#8212;but when you have the
+							cloud backing up your data, surviving disasters great and small
+							is a whole lot easier. Watch the video to find out more.</p>
 					</div>
 				</div>
 				<div class="video-left col-sm-7 visible-xs">
-					<iframe width="560" height="315" src="//www.youtube.com/embed/PfOEqfbQ6gE" frameBorder="0" allowfullscreen></iframe>
+					<iframe width="560" height="315"
+						src="//www.youtube.com/embed/PfOEqfbQ6gE" frameBorder="0"
+						allowfullscreen></iframe>
 				</div>
 			</div>
 		</div>
--- business/data-backup/index.php
+++ business/data-backup/index.php
@@ -1,86 +1,109 @@
 <?php
-	#############
-	# THIS PART IS PAGE LOADER
-	#############
-	
-	include_once dirname(__FILE__) . "/../../config.php";
-	define_root_url(__FILE__);
-	
-	#############
-	# END PAGE LOADER
-	#############
+#############
+# THIS PART IS PAGE LOADER
+#############
+
+include_once dirname(__FILE__) . "/../../config.php";
+define_root_url(__FILE__);
+
+#############
+# END PAGE LOADER
+#############
 ?>
 
 <?php
-	page_header("CenturyLink Cloud | Business Owner | Data Backup");
+page_header("CenturyLink Cloud | Business Owner | Data Backup");
 ?>
 
 <div id="business-db">
 	<div class="hero hidden-xs">
-	<div class="container wrapper">
-		<div class="hero-container">
-			<div class="hero-left">
-				<img class="hero-label" src="<?php echo ROOT_URL;  ?>/images/businessdb-label.jpg" alt="DATA BACKUP"/>
-				<div class="hero-left-content">
-					<p class="headline">Two steps to backing up data.<br/> Turning it on. And forgetting it.</p>
-					<h3 class="offer">Try CenturyLink Cloud FREE <br/> for 30 Days.* </h3>
-					<p class="call"><a href="tel:800.361.0833">Call 800.361.0833</a></p>
+		<div class="container wrapper">
+			<div class="hero-container">
+				<div class="hero-left">
+					<img class="hero-label"
+						src="<?php echo ROOT_URL;  ?>/images/businessdb-label.jpg"
+						alt="DATA BACKUP" />
+					<div class="hero-left-content">
+						<p class="headline">
+							Two steps to backing up data.<br /> Turning it on. And forgetting
+							it.
+						</p>
+						<h3 class="offer">
+							Try CenturyLink Cloud FREE <br /> for 30 Days.*
+						</h3>
+						<p class="call">
+							<a href="tel:800.361.0833">Call 800.361.0833</a>
+						</p>
+					</div>
+				</div>
+				<div class="hero-right">
+					<img class="icon"
+						src="<?php echo ROOT_URL;  ?>/images/businessdb-icon.png"
+						alt="DATA BACKUP" />
 				</div>
 			</div>
-			<div class="hero-right">
-				<img class="icon" src="<?php echo ROOT_URL;  ?>/images/businessdb-icon.png" alt="DATA BACKUP"/>
+		</div>
+	</div>
+
+	<div class="mobile-hero visible-xs">
+		<div class="main-hero">
+			<img
+				src="<?php echo ROOT_URL;  ?>/images/mobile-business-db-hero.png"
+				alt="" />
+		</div>
+		<div class="container wrapper">
+			<div class="padding-fix">
+				<div class="mobile-hero-content">
+					<p class="headline">Two steps to backing up data. Turning it on.
+						And forgetting it.</p>
+					<h3 class="offer">Try CenturyLink Cloud FREE for 30 Days.*</h3>
+					<p class="call">
+						<a href="tel:800.361.0833">Call 800.361.0833</a>
+					</p>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
-<div class="mobile-hero visible-xs">
-	<div class="main-hero">
-		<img src="<?php echo ROOT_URL;  ?>/images/mobile-business-db-hero.png" alt=""/>
+
+	<div id="sticky-nav-wrap" class="sticky-nav">
+		<div class="container wrapper">
+			<div class="padding-fix">
+				<a href="http://www.centurylink.com/small-business/" target="_blank"
+					class="sticky-logo hidden-sm hidden-xs"> <object class="nav-logo"
+						data="<?php echo ROOT_URL;?>/images/nav-logo.svg" width="141"
+						height="30" type="image/svg+xml">
+						<img src="<?php echo ROOT_URL;  ?>/images/nav-logo.png"
+							alt="CENTURYLINK" width="141" height="30" />
+					</object> </a>
+				<ul class="multi" id="sticky-nav">
+					<li><a href="#easy">Easy Management</a></li>
+					<li><a href="#safe">Safe & Available</a></li>
+					<li><a href="#ffb">FastForward onBoarding</a></li>
+					<li><a href="#pricing">Pricing</a></li>
+				</ul>
+				<div class="fix-center hidden-sm hidden-xs call">
+					<a href="tel:800.361.0833">Call 800.361.0833</a>
+				</div>
+			</div>
+		</div>
+
 	</div>
-	<div class="container wrapper">
-		<div class="padding-fix">
-			<div class="mobile-hero-content">
-				<p class="headline">Two steps to backing up data. Turning it on. And forgetting it.</p>
-				<h3 class="offer">Try CenturyLink Cloud FREE  for 30 Days.* </h3>
-				<p class="call"><a href="tel:800.361.0833">Call 800.361.0833</a></p>
+
+
+	<div class="why-ctl">
+		<div class="container wrapper">
+			<div class="padding-fix">
+				<p class="why-ctl-content">
+					Not only simple, but effective. CenturyLink Data Backup means
+					never worrying <br /> about backing up critical information again.
+					It also means being able to retrieve files, organize them <br />and
+					restore data with the press of a button.
+				</p>
 			</div>
 		</div>
 	</div>
-</div>
 
-
-<div id="sticky-nav-wrap" class="sticky-nav">
-	<div class="container wrapper">
-		<div class="padding-fix">
-			<a href="http://www.centurylink.com/small-business/" target="_blank" class="sticky-logo hidden-sm hidden-xs">
-				<object class="nav-logo" data="<?php echo ROOT_URL;?>/images/nav-logo.svg" width="141" height="30" type="image/svg+xml">
-					<img src="<?php echo ROOT_URL;  ?>/images/nav-logo.png" alt="CENTURYLINK" width="141" height="30"/>
-				</object>
-			</a>
-			<ul class="multi" id="sticky-nav">
-				<li><a href="#easy">Easy Management</a></li>
-				<li><a href="#safe">Safe & Available</a></li>
-				<li><a href="#ffb">FastForward onBoarding&#8480;</a></li>
-				<li><a href="#pricing">Pricing</a></li>
-			</ul>
-			<div class="fix-center hidden-sm hidden-xs call"><a href="tel:800.361.0833">Call 800.361.0833</a></div>
-		</div>
-	</div>
-	
-</div>
-
-
-<div class="why-ctl">
-	<div class="container wrapper">
-	<div class="padding-fix">
-		<p class="why-ctl-content">
-			Not only simple, but effective. CenturyLink Data Back up means never worrying <br/> about backing up critical information again. It also means being able to retrieve files, organize them <br/>and restore data with the press of a button.
-		</p>
-	</div>
-	</div>
-</div>
-	
 	<div id="bdata">
 		<?php page_inc("business/data-backup/b-data.php"); ?>
 	</div>
