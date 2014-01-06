a9ffecf0b0358cee7f3f787142512546087d8baf
Nam Tran Huu <namth@hdwebsoft.com>
2014-01-06 10:33:46 +0700

    Business Owner page
    Business Owner page copy  Intro paragraph - "1000" should be “1,000"

--- business/index.php
+++ business/index.php
@@ -74,7 +74,7 @@
 	<div class="container wrapper">
 	<div class="padding-fix">
 		<p class="why-ctl-content">
-			Just because yours business doesn’t have 1000 employees doesn’t mean<br/>
+			Just because yours business doesn’t have 1,000 employees doesn’t mean<br/>
 			you can’t perform like you do. CenturyLink provides the latest cloud-based software<br/>
 			and solutions to help you do just that – easily and cost-effectively.<br/>
 			The only thing you’ll be missing is the headache.
